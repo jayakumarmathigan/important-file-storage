39c398b4ec90f2037276a2bca9a610e50eb5f46f
jayakumarmathigan <33954638+jayakumarmathigan@users.noreply.github.com>
2018-02-05 19:10:38 +0530

    Add files via upload

--- reset.php
+++ reset.php
@@ -46,12 +46,15 @@ if($con->connect_error){
 <body>
 <div class="login-form">
   <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
-        <h2 class="text-center">Checking Details</h2>
+  <marquee><h2 class="text-center">Checking Details</h2></marquee> 
         <div class="form-group">
             <input type="password" class="form-control" placeholder="Old Password" name="old_pass">
         </div>
         <div class="form-group">
             <input type="password" class="form-control" placeholder="New Password" name="new_pass">
+        </div>
+		<div class="form-group">
+            <input type="password" class="form-control" placeholder="Confirm Password" name="new_pass1">
         </div>
         <div class="form-group">
             <button type="submit" class="btn btn-primary btn-block" name="submit">Save</button>
@@ -65,12 +68,23 @@ if($con->connect_error){
 if(isset($_POST['submit'])){
 	$old_pass = $_POST['old_pass'];
 	$new_pass = $_POST['new_pass'];
-	$sql = "UPDATE signup SET pass='$new_pass' WHERE pass='$old_pass'";
-	if($con->query($sql)){
-		header('location:login.php');
-	}else{
-		echo "<p id='hide'>Sorry,password doesn't match</p>";
+	$new_pass1 = $_POST['new_pass1'];
+	$sql = "SELECT pass FROM signup WHERE pass='$old_pass'";
+	$result = $con->query($sql);
+	if($result->num_rows>0){
+		if($new_pass == $new_pass1){
+			$sql1 = "UPDATE signup SET pass='$new_pass' WHERE pass='$old_pass'";
+			if($con->query($sql1))
+			{
+			header('location:login.php');
+		}else{
+			echo "<p id='hide'>Sorry,password doesn't match</p>";
+		}
+		}else{
+			echo "<p id='hide'>Sorry,password doesn't match</p>";
+		}
 	}
+	
 }
 ?>
 <script type="text/javascript">
